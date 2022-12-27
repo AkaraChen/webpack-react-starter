e144987b1a1999e6527a647a65b14a2fb6b1ea6f
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-27 11:53:56 +0800

    feat: configure mfsu

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,53 +5,80 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCSSExtract from 'mini-css-extract-plugin';
 
 const mfsu = new MFSU({
+    strategy: 'normal',
     implementor: webpack,
-    depBuildConfig: {},
-    startBuildWorker: undefined as any,
+    depBuildConfig: null,
+    startBuildWorker: null as any,
     buildDepWithESBuild: true
 });
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry: './src/main.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(`ts|tsx)$/,
-                loader: 'swc-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCSSExtract.loader,
-                    'css-loader',
-                    'postcss-loader'
-                ]
+export default async () => {
+    const config: webpack.Configuration = {
+        mode: 'development',
+        entry: './src/main.tsx',
+        // @ts-ignore
+        devServer: {
+            onBeforeSetupMiddleware(devServer: any) {
+                for (const middleware of mfsu.getMiddlewares()) {
+                    devServer.app.use(middleware);
+                }
             }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    optimization: {
-        minimizer: [
-            new ESBuildMinifyPlugin({
-                legalComments: 'none',
-                css: true
-            })
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './index.html'
-        }),
-        new MiniCSSExtract()
-    ],
-    stats: 'minimal'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(`ts|tsx)$/,
+                    loader: 'swc-loader'
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        MiniCSSExtract.loader,
+                        'css-loader',
+                        'postcss-loader'
+                    ]
+                },
+                {
+                    test: /\.[jt]sx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                            plugins: [
+                                ...mfsu.getBabelPlugins()
+                            ]
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        optimization: {
+            minimizer: [
+                new ESBuildMinifyPlugin({
+                    legalComments: 'none',
+                    css: true
+                })
+            ]
+        },
+        plugins: [
+            new HTMLWebpackPlugin({
+                template: './index.html'
+            }),
+            new MiniCSSExtract()
+        ],
+        stats: 'minimal',
+        cache: {
+            type: 'filesystem'
+        }
+    };
+
+    const depConfig: webpack.Configuration = {};
+
+    await mfsu.setWebpackConfig({ config, depConfig });
+
+    return config;
 };
-
-const depConfig: webpack.Configuration = {};
-
-mfsu.setWebpackConfig({ config, depConfig });
-
-export default config;
