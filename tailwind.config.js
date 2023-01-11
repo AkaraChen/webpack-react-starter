55b7194b7d0bc5201620398d5e65b3818542d1b9
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-01-11 21:52:13 +0800

    feat: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const typography = require('@tailwindcss/typography');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,ts,jsx,tsx}'],
-    theme: {
-        extend: {}
-    },
-    plugins: []
+  content: ['./src/**/*.{html,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [typography]
 };
