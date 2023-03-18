6af4b49aa85edf6abc875087e6c395a4658269ed
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-03-18 18:22:52 +0800

    chore: update deps

--- main.tsx
+++ main.tsx
@@ -1,12 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './styles/global.css';
 import App from './pages/_app';
+import { StrictMode } from 'react';
 
 const container = document.querySelector('#app');
 const root = createRoot(container!);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { router } from '@/routes';
 import { RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
